ee6a1cb3aeef63761336d356575de6ad4322e8a2
bgulla <hey@bgulla.dev>
2023-08-15 07:35:18 -0400

    and here we go

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/brandon/src/rfed/hauler-docs/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/brandon/src/rfed/hauler-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/brandon/src/rfed/hauler-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/brandon/src/rfed/hauler-docs/src/css/custom.css'),
+];
---
+++ docs/installation/installation.md
@@ -0,0 +1,3 @@
+## Installation
+
+The latest version of `hauler` is available as statically compiled binaries for most combinations of operating systems and architectures on the GitHub [releases](https://github.com/rancherfederal/hauler/releases) page.
